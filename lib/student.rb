f0ba4cf1c14c1d99bec20a1d00f591fcd1d55fcc
Kristen Peck <kpeck71@gmail.com>
2017-10-16 20:06:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,9 +4,10 @@ class Student > User
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
-    knowledge = []
+    @knowledge = []
   end
 
-
+  def learn(knowledge)
+    @knowledge << knowledge
 
 end
