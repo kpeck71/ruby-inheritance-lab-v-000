6b577e2f4e4dde3b7f26e4feee8294ff6f3e647e
Kristen Peck <kpeck71@gmail.com>
2017-10-16 20:06:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,12 @@
 class Student > User
-  attr_accessor :first_name, :last_name
+  attr_accessor :first_name, :last_name, :knowledge
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
     knowledge = []
   end
-  
+
+
+
 end
